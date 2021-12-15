4f86ea8bcaaaddb2bb4f06a4f785e2c3c6d0b7cb
nikolskiyv <50186930+nikolskiyv@users.noreply.github.com>
2021-12-15 08:52:52 +0300

    Add WPF.cs

--- Component/WPF.cs
+++ Component/WPF.cs
@@ -1,1 +1,46 @@
-﻿
+﻿using System.Collections.Immutable;
+using System.ComponentModel;
+using YOLOv4MLNet;
+
+namespace PerceptionComponent
+{
+    public class WPFView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        Model model = new Model();
+        public WPFView()
+        {
+            model = new Model();
+            PropertyChanged += q;
+            model.PropertyChanged += (s, e) =>
+            {
+                NotifyVM(e.PropertyName);
+            };
+        }
+        private void NotifyVM(string name)
+        {
+            OnPropertyChanged(name);
+        }
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        private void q(object sender, PropertyChangedEventArgs e)
+        {
+            return;
+        }
+        public ImmutableDictionary<string, int> ClassCount => model.ClassCount;
+        public ImmutableDictionary<string, ImmutableList<string>> ClassImages => model.ClassImages;
+        public ImmutableList<string> ImagesInProcess => model.ImagesInProcess;
+        public Model Model
+        {
+            get
+            {
+                return model;
+            }
+        }
+    }
+}
