5178d72922a3ee4e88f96a37a01dc235d9e54297
nikolskiyv <50186930+nikolskiyv@users.noreply.github.com>
2021-12-15 08:41:28 +0300

    Lab2

--- Component/Model.cs
+++ Component/Model.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Collections.Immutable;
 
 namespace YOLOv4MLNet
 {
@@ -8,25 +9,49 @@ namespace YOLOv4MLNet
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        readonly ConcurrentDictionary<string, int> classDict;
-        readonly ConcurrentBag<ImageInfo> imgBag;
+        ImmutableDictionary<string, int> classDict;
+        ImmutableDictionary<string, int> classCount;
+        ImmutableDictionary<string, ImmutableList<string>> classImages;
+        ImmutableList<string> imagesInProcess;
+
+        ConcurrentBag<ImageInfo> imgBag;
 
         public Model()
         {
             imgBag = new ConcurrentBag<ImageInfo>();
-            classDict = new ConcurrentDictionary<string, int>();
-
-            for (int i = 0; i < Recognition.classesNames.Length; ++i)
-            {
-                classDict.TryAdd(Recognition.classesNames[i], 0);
-            }
+            classCount = ImmutableDictionary<string, int>.Empty;
+            classImages = ImmutableDictionary<string, ImmutableList<string>>.Empty;
+            imagesInProcess = ImmutableList<string>.Empty;
         }
 
-        public ConcurrentDictionary<string, int> ClassDict
+        public void RemoveImage(int ind)
+        {
+            lock (imagesInProcess)
+            {
+                imagesInProcess = imagesInProcess.RemoveAt(ind);
+                OnPropertyChanged(nameof(ImagesInProcess));
+            }
+        }
+        public void InsertImage(int idx, string pathToImage)
+        {
+            lock (imagesInProcess)
+            {
+                imagesInProcess = imagesInProcess.Insert(idx, pathToImage);
+                OnPropertyChanged(nameof(ImagesInProcess));
+            }
+        }
+        public ImmutableDictionary<string, int> ClassCount
         {
             get
             {
                 return classDict;
+            }
+        }
+        public ImmutableDictionary<string, ImmutableList<string>> ClassImages
+        {
+            get
+            {
+                return classImages;
             }
         }
 
@@ -38,7 +63,7 @@ namespace YOLOv4MLNet
             }
         }
 
-        public void Add(ImageInfo info)
+        public void Add(ImageInfo info, string pathToImage)
         {
             lock (imgBag)
             {
@@ -46,18 +71,52 @@ namespace YOLOv4MLNet
 
                 foreach (ObjectInfo item in info.ObjBag)
                 {
-                    ++classDict[item.Name];
+                    if (classCount.ContainsKey(o.Cls))
+                    {
+                        classCount = classCount.SetItem(o.Cls, classCount[o.Cls] + 1);
+                    }
+                    else
+                    {
+                        classCount = classCount.Add(o.Cls, 1);
+                    }
+
+                    if (ClassImages.ContainsKey(o.Cls))
+                    {
+                        if (!classImages[o.Cls].Contains(pathToImage))
+                        {
+                            classImages = classImages.SetItem(o.Cls, classImages[o.Cls].Add(pathToImage));
+                        }
+                    }
+                    else
+                    {
+                        classImages = classImages.Add(o.Cls, (ImmutableList<string>.Empty).Add(pathToImage));
+                    }
                 }
 
-                OnPropertyChanged();
+                OnPropertyChanged(nameof(ClassDict));
+            }
+        }
+        public void AddImage(string pathToImage)
+        {
+            lock (imagesInProcess)
+            {
+                imagesInProcess = imagesInProcess.Add(pathToImage);
+                OnPropertyChanged(nameof(ImagesInProcess));
             }
         }
 
-        private void OnPropertyChanged([CallerMemberName] string prop = "")
+        public ImmutableList<string> ImagesInProcess
+        {
+            get
+            {
+                return imagesInProcess;
+            }
+        }
+        public void OnPropertyChanged(string name)
         {
             if (PropertyChanged != null)
             {
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
             }
         }
     }
